34dc101aba13c46198683d19ce43b60aebeb2764
Sayam753 <sayamkumar753@yahoo.in>
2020-05-08 08:34:44 +0530

    Understood map_nested and merge_dict functions

---
+++ utils.py
@@ -0,0 +1,36 @@
+'''
+If a finally clause is present, the finally clause will execute as the 
+last task before the try statement completes.
+'''
+
+
+def fun(a, b):
+    print("Inside function")
+    try:
+        return a/b
+    except ZeroDivisionError:
+        print("ZeroDivisionError")
+    else:
+        print("Inside else")
+    finally:
+        print("Inside the finally")
+
+    
+def merge(*args, **kwargs):
+    print(type(args))
+    print(type(kwargs))
+    for mappable in args:
+        if set(mappable) & set(kwargs):
+            raise ValueError(
+                "Found duplicate keys in merge: {}".format(set(mappable) & set(kwargs))
+            )
+        kwargs.update(mappable)
+    return kwargs
+
+if __name__ == "__main__":
+    print(fun(10, 0))
+    print(fun(20, 30))
+
+    a = {1: 2, 3: 4}
+    b = {10: 20, 11:22}
+    merge(a, b)
