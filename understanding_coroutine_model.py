b3703844a7e9ba136cb7b7e5ed761f64734faf0a
Sayam753 <sayamkumar753@yahoo.in>
2020-05-10 21:02:29 +0530

    Started coroutine_model

---
+++ understanding_coroutine_model.py
@@ -0,0 +1,6 @@
+'''
+Learnings - 
+1. types.MappingProxyType - 
+It is used as a read only dict. Find more here https://github.com/topper-123/Articles/blob/master/New-interesting-data-types-in-Python3.rst
+
+'''
